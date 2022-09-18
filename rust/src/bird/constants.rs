b0e4d1ae0eaa8f83d6d9807578a71babc9cd9eb3
Slazaa <afitero@gmail.com>
2022-09-18 15:28:19 +0200

    Change import to use

--- rust/src/bird/constants.rs
+++ rust/src/bird/constants.rs
@@ -14,13 +14,13 @@ pub static OPERATORS: [&str; 34] = [
 pub static OPERATOR_CHARS: &str = "+-*/%=!><&|^~:";
 
 pub static KEYWORDS: [&str; 19] = [
-	"as"    , "break"   , "case",
-	"const" , "continue", "else",
-	"enum"  , "func"    , "if"  ,
-	"impl"  , "import"  , "loop",
-	"match" , "mut"     , "pub" ,
-	"return", "struct"  , "type",
-	"var"   ,
+	"as"    , "break"   , "case"  ,
+	"const" , "continue", "else"  ,
+	"enum"  , "func"    , "if"    ,
+	"impl"  , "loop"    , "match" ,
+	"mut"   , "pub"     , "return",
+	"struct", "type"    , "use"   ,
+	"var"
 ];
 
 pub mod compile {
