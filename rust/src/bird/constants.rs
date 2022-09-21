d9c4a75e5b5a763729b39c32ae274eb2092bd15d
Slazaa <afitero@gmail.com>
2022-09-21 17:53:19 +0200

    Remove case keyword

--- rust/src/bird/constants.rs
+++ rust/src/bird/constants.rs
@@ -13,13 +13,13 @@ pub static OPERATORS: [&str; 34] = [
 
 pub static OPERATOR_CHARS: &str = "+-*/%=!><&|^~:";
 
-pub static KEYWORDS: [&str; 18] = [
-	"break"   , "case"  , "const" ,
-	"continue", "else"  , "enum"  ,
-	"func"    , "if"    , "impl"  ,
-	"loop"    , "match" , "mut"   ,
-	"pub"     , "return", "struct",
-	"type"    , "use"   , "var"
+pub static KEYWORDS: [&str; 17] = [
+	"break" , "const" , "continue",
+	"else"  , "enum"  , "func"    ,
+	"if"    , "impl"  , "loop"    ,
+	"match" , "mut"   , "pub"     ,
+	"return", "struct", "type"    ,
+	"use"   , "var"
 ];
 
 pub mod compile {
