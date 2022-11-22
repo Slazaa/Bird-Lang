81760b9c9b7fdc505a246a4b34537e48927caf5a
Slazaa <afitero@gmail.com>
2022-11-22 20:00:13 +0100

    add(var_decl): type precision

--- rust/parser/src/patterns/var_decl.rs
+++ rust/parser/src/patterns/var_decl.rs
@@ -7,12 +7,15 @@ use super::Expr;
 #[derive(Debug, Clone)]
 pub struct VarDecl {
 	pub id: String,
+	pub var_type: Option<String>,
 	pub val: Option<Expr>
 }
 
-pub static VAR_DECL_PATTERNS: [(&str, &str, PatternFunc<Node>); 2] = [
+pub static VAR_DECL_PATTERNS: [(&str, &str, PatternFunc<Node>); 4] = [
 	("var_decl", "VAR ID SEMI", var_decl),
-	("var_decl", "VAR ID EQ expr SEMI", var_decl_expr)
+	("var_decl", "VAR ID EQ expr SEMI", var_decl_expr),
+	("var_decl", "VAR ID COL ID SEMI", var_decl_typed),
+	("var_decl", "VAR ID COL ID EQ expr SEMI", var_decl_typed_expr)
 ];
 
 fn var_decl(nodes: &[Node]) -> Result<Node, String> {
@@ -21,7 +24,7 @@ fn var_decl(nodes: &[Node]) -> Result<Node, String> {
 		_ => return Err(format!("Invalid node '{:?}' in 'var'", nodes[1]))
 	};
 
-	Ok(Node::VarDecl(VarDecl { id, val: None }))
+	Ok(Node::VarDecl(VarDecl { id, var_type: None, val: None }))
 }
 
 fn var_decl_expr(nodes: &[Node]) -> Result<Node, String> {
@@ -35,5 +38,38 @@ fn var_decl_expr(nodes: &[Node]) -> Result<Node, String> {
 		_ => return Err(format!("Invalid node '{:?}' in 'var'", nodes[3]))
 	};
 
-	Ok(Node::VarDecl(VarDecl { id, val }))
+	Ok(Node::VarDecl(VarDecl { id, var_type: None, val }))
+}
+
+fn var_decl_typed(nodes: &[Node]) -> Result<Node, String> {
+	let id = match &nodes[1] {
+		Node::Token(token) if token.name() == "ID" => token.symbol().to_owned(),
+		_ => return Err(format!("Invalid node '{:?}' in 'var'", nodes[1]))
+	};
+
+	let var_type = match &nodes[3] {
+		Node::Token(token) if token.name() == "ID" => token.symbol().to_owned(),
+		_ => return Err(format!("Invalid node '{:?}' in 'var'", nodes[1]))
+	};
+
+	Ok(Node::VarDecl(VarDecl { id, var_type: Some(var_type), val: None }))
+}
+
+fn var_decl_typed_expr(nodes: &[Node]) -> Result<Node, String> {
+	let id = match &nodes[1] {
+		Node::Token(token) if token.name() == "ID" => token.symbol().to_owned(),
+		_ => return Err(format!("Invalid node '{:?}' in 'var'", nodes[1]))
+	};
+
+	let var_type = match &nodes[3] {
+		Node::Token(token) if token.name() == "ID" => token.symbol().to_owned(),
+		_ => return Err(format!("Invalid node '{:?}' in 'var'", nodes[1]))
+	};
+
+	let val = match &nodes[5] {
+		Node::Expr(expr) => Some(expr.to_owned()),
+		_ => return Err(format!("Invalid node '{:?}' in 'var'", nodes[3]))
+	};
+
+	Ok(Node::VarDecl(VarDecl { id, var_type: Some(var_type), val }))
 }
