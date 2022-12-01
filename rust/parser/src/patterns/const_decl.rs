439ff51c3b61ba8cb27eb329b5b739b0bcfd6364
Slazaa <afitero@gmail.com>
2022-12-01 18:03:13 +0100

    add(patterns): add const_decl pattern

--- rust/parser/src/patterns/const_decl.rs
+++ rust/parser/src/patterns/const_decl.rs
@@ -14,9 +14,8 @@ pub struct ConstDecl {
 	pub loc: Loc
 }
 
-pub static CONST_DECL_PATTERNS: [(&str, &str, PatternFunc<Node, Feedback>); 3] = [
+pub static CONST_DECL_PATTERNS: [(&str, &str, PatternFunc<Node, Feedback>); 2] = [
 	("const_decl", "CONST ID EQ expr SEMI", const_decl),
-	("const_decl", "CONST ID COL type SEMI", const_decl_typed),
 	("const_decl", "CONST ID COL type EQ expr SEMI", const_decl_typed_expr)
 ];
 
@@ -35,23 +34,6 @@ fn const_decl(nodes: &[Node]) -> Result<Node, Feedback> {
 	loc.end = nodes[4].token().unwrap().loc.end.to_owned();
 
 	Ok(Node::ConstDecl(ConstDecl { id, var_type: None, val, loc }))
-}
-
-fn const_decl_typed(nodes: &[Node]) -> Result<Node, Feedback> {
-	let id = match &nodes[1] {
-		Node::Token(token) if token.name == "ID" => token.symbol.to_owned(),
-		_ => panic!("If you see this, that means the dev does bad work")
-	};
-
-	let var_type = match &nodes[3] {
-		Node::Type(x) => x.to_owned(),
-		_ => panic!("If you see this, that means the dev does bad work")
-	};
-
-	let mut loc = nodes[0].token().unwrap().loc.to_owned();
-	loc.end = nodes[4].token().unwrap().loc.end.to_owned();
-
-	Ok(Node::ConstDecl(ConstDecl { id, var_type: Some(var_type), val: None, loc }))
 }
 
 fn const_decl_typed_expr(nodes: &[Node]) -> Result<Node, Feedback> {
