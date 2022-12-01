49fe0b2a5571d4c45fc861470e3397d8fcd5c574
Slazaa <afitero@gmail.com>
2022-12-01 14:29:40 +0100

    update(patterns): added type ptr pattern

--- rust/parser/src/patterns/type_.rs
+++ rust/parser/src/patterns/type_.rs
@@ -11,8 +11,9 @@ pub struct Type {
 	pub loc: Loc
 }
 
-pub static TYPE_PATTERNS: [(&str, &str, PatternFunc<Node, Feedback>); 1] = [
-	("type", "ID", type_)
+pub static TYPE_PATTERNS: [(&str, &str, PatternFunc<Node, Feedback>); 2] = [
+	("type", "ID", type_),
+	("type", "MULT ID", type_ptr)
 ];
 
 pub fn type_(nodes: &[Node]) -> Result<Node, Feedback> {
@@ -25,4 +26,16 @@ pub fn type_(nodes: &[Node]) -> Result<Node, Feedback> {
 	loc.end = nodes[0].token().unwrap().loc.end.to_owned();
 
 	Ok(Node::Type(Type { id, ptr: false, loc }))
+}
+
+pub fn type_ptr(nodes: &[Node]) -> Result<Node, Feedback> {
+	let id = match &nodes[1] {
+		Node::Token(token) if token.name == "ID" => token.symbol.to_owned(),
+		_ => panic!("If you see this, that means the dev does bad work")
+	};
+
+	let mut loc = nodes[0].token().unwrap().loc.to_owned();
+	loc.end = nodes[1].token().unwrap().loc.end.to_owned();
+
+	Ok(Node::Type(Type { id, ptr: true, loc }))
 }
