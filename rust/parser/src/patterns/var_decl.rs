5e94f9a19808ff05047176027a25633dec92eb91
Slazaa <afitero@gmail.com>
2022-12-02 21:45:53 +0100

    add(var_decl): add pattern

--- rust/parser/src/patterns/var_decl.rs
+++ rust/parser/src/patterns/var_decl.rs
@@ -14,24 +14,22 @@ pub struct VarDecl {
 	pub loc: Loc
 }
 
-pub static VAR_DECL_PATTERNS: [(&str, &str, PatternFunc<Node, Feedback>); 1] = [
-	//("var_decl", "VAR ID SEMI", var_decl),
+pub static VAR_DECL_PATTERNS: [(&str, &str, PatternFunc<Node, Feedback>); 2] = [
+	("var_decl", "VAR ID SEMI", var_decl),
 	("var_decl", "VAR ID EQ expr SEMI", var_decl_expr),
 ];
 
-/*
 fn var_decl(nodes: &[Node]) -> Result<Node, Feedback> {
 	let id = match &nodes[1] {
 		Node::Token(token) if token.name == "ID" => token.symbol.to_owned(),
-		_ => return Err(format!("Invalid node '{:?}' in 'var_decl'", nodes[1]))
+		_ => panic!("If you see this, that means the dev does bad work")
 	};
 
 	let mut loc = nodes[0].token().unwrap().loc.to_owned();
 	loc.end = nodes[2].token().unwrap().loc.end.to_owned();
 
-	Ok(Node::VarDecl(VarDecl { id, var_type: None, val: None, loc }))
+	Ok(Node::VarDecl(VarDecl { public: false, id, val: None, loc }))
 }
-*/
 
 fn var_decl_expr(nodes: &[Node]) -> Result<Node, Feedback> {
 	let id = match &nodes[1] {
