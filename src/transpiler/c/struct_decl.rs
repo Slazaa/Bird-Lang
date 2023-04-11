b418b003d2e1785e5c14628b9c98583bab2f2e2d
Slazaa <slazaa42@gmail.com>
2023-04-11 16:11:40 +0200

    Add anonymous fields for struct decl

--- src/transpiler/c/struct_decl.rs
+++ src/transpiler/c/struct_decl.rs
@@ -1,7 +1,14 @@
 use crate::{parser::exprs::struct_decl::{StructDecl, Field}, transpiler::c::{r#type, ident}};
 
-pub fn transpile_field(input: &Field) -> String {
-	let ident = ident::transpile(input.ident.as_ref().unwrap());
+pub fn transpile_field(input: &Field, next_anon_field: &mut i32) -> String {
+	let ident = if let Some(ident) = &input.ident {
+		ident::transpile(ident)
+	} else {
+		let res = format!("{}", next_anon_field) + "_";
+		*next_anon_field += 1;
+		res 
+	};
+
 	let r#type = r#type::transpile(&input.r#type);	
 	
 	format!("{type} {ident}")
@@ -9,9 +16,10 @@ pub fn transpile_field(input: &Field) -> String {
 
 pub fn transpile_fields(input: &[Field]) -> String {
 	let mut res = "{".to_owned();
+	let mut next_anon_field = 0;
 
 	for field in input {
-		res += &format!("{};", transpile_field(field));
+		res += &format!("{};", transpile_field(field, &mut next_anon_field));
 	}
 
 	res += "}";
